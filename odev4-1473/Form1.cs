107ae1f5da3cd224a20826eaee9567a3fe736621
sukeynacelik1473 <145451091+sukeynacelik1473@users.noreply.github.com>
2023-11-27 20:24:09 +0300

    Add files via upload

---
+++ odev4-1473/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev4_1473
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int say = 0;
+            do
+            {
+                richTextBox1.Text = richTextBox1.Text + say.ToString();
+                say++;
+            } while (say <= 30);
+        }
+    }
+}
